1588ee04866a49fc091336c28b8c91c37427e7b6
Elliott Nottingham <elliottnottingham07@gmail.com>
2024-11-13 19:59:37 -0600

    created CRUD for our inventory.js on our backend server

---
+++ backEnd/models/inventory.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const inventorySchema = new Schema({
+
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    description: {
+        type: String,
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+});
+
+export default model ("Inventory", inventorySchema)
